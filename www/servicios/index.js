19f9c22aa9e08ff2cdd02c505971e6bc8bb8e3d2
dianni041004-droid <dianni041004@gmail.com>
2026-07-11 23:04:01 -0400

    Delete www directory

--- www/servicios/index.js
+++
@@ -1,6 +0,0 @@
-// servicios/index.js
-// Punto de entrada para todos los servicios
-
-export * from './ServicioNotificaciones.js';
-export * from './ServicioNotificacionesWeb.js';
-export * from './ServicioNotificacionesAndroid.js';
